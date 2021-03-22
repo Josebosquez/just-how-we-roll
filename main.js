4a11ef0a36a349021b87d4eef3aeecca410279dd
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-03-22 11:09:52 -0600

    updated imgs srcs

--- main.js
+++ main.js
@@ -30,14 +30,43 @@ const sortByNumber = function(arr) {
 /*******************
  * YOUR CODE BELOW *
  *******************/
+const D6Img = document.getElementById("d6-roll");
+  D6Img.src = "images/start/d6.png"
 
+const DD6Img1 = document.getElementById("double-d6-roll-1");
+  DD6Img1.src = "images/start/d6.png"
 
+const DD6Img2 = document.getElementById("double-d6-roll-2");
+  DD6Img2.src = "images/start/d6.png"
+
+const D12Img = document.getElementById("d12-roll");
+  D12Img.src = "images/start/d12.jpeg"
+
+const D20Img = document.getElementById("d20-roll");
+  D20Img.src = "images/start/d20.jpg"
 
 /*******************
  * EVENT LISTENERS *
  *******************/
+const clickD6 = document.querySelector("#d6-roll");
+
+function rollingtheD6 (){
+
+}
+
+clickD6.addEventListener('click', somefunction);
 
 
+const clickDD6 = document.querySelector("#double img")
+clickDD6.addEventListener('click', somefunction);
+
+
+const clickD12 = document.querySelector("#d12-roll")
+clickD12.addEventListener('click', somefunction);
+
+
+const clickD20 = document.querySelector("#d20-roll")
+clickD20.addEventListener('click', somefunction);
 
 
 /******************
@@ -55,3 +84,4 @@ const sortByNumber = function(arr) {
 /****************
  * MATH SECTION *
  ****************/
+
